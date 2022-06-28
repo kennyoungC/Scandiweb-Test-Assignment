37312473a321b40207d52af196912c3572f29bf8
Kenneth <obikennethe913@gmail.com>
2022-06-28 11:05:48 +0300

    persist data after reload

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import { Provider } from "react-redux"
-import store from "./store"
+import { store, persistor } from "./store"
 import { BrowserRouter } from "react-router-dom"
+import { PersistGate } from "redux-persist/integration/react"
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
   </BrowserRouter>
   //
 )
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -3,14 +3,36 @@ import productReducer from "./reducers/ProductReducer"
 import currencyReducer from "./reducers/CurrencyReducer"
 import CartReducer from "./reducers/CartReducer"
 
+import { persistStore, persistReducer } from "redux-persist"
+import storageSession from "redux-persist/lib/storage/session"
+import { encryptTransform } from "redux-persist-transform-encrypt"
+
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_NAME,
+      onError: (error) => {
+        console.log(error)
+      },
+    }),
+  ],
+}
+
 const rootReducer = combineReducers({
   productList: productReducer,
   currency: currencyReducer,
   cart: CartReducer,
 })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const store = configureStore({
-  reducer: rootReducer,
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
 
-export default store
+export const persistor = persistStore(store)
